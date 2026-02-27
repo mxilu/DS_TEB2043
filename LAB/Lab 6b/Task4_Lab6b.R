69de8f2ba716c84f5d77c6ace3b18f97ae523a34
mxilu <umaiswan12@gmail.com>
2026-02-27 11:05:17 +0800

    Added changes to Task 6

--- LAB/Lab 6b/Task4_Lab6b.R
+++ LAB/Lab 6b/Task4_Lab6b.R
@@ -3,5 +3,10 @@ studentdf$score <- as.numeric(studentdf$score)
 studentdf$attempts <- as.numeric(studentdf$attempts)
 studentdf$qualify <- as.factor(studentdf$qualify)
 
+cat("Structure of data frame:")
+str(studentdf)
+cat("Summary of data frame:")
 summary(studentdf)
+cat("No of rows: ", nrow(studentdf), "\nNo of columns: ", ncol(studentdf))
+
 
